b3215c63c04c42455352b5a08ab1662b8becc2b7
ETR <49214324+etorresr@users.noreply.github.com>
2023-03-15 13:39:51 -0600

    Update ejer1.py

--- ejer1.py
+++ ejer1.py
@@ -6,14 +6,14 @@ https://codeloop.org/python-firebase-course-for-beginners/
 import pyrebase
 
 config = {
-"apiKey": "AIzaSyAjjTHMIV0y394tayvijhU-aVVcKdkIZxU",
-"authDomain": "esp32-aleatorios.firebaseapp.com",
-"databaseURL": "https://esp32-aleatorios-default-rtdb.firebaseio.com",
+"apiKey": "AIcKdkIZxU",
+"authDomain": "esp32-ios.firebaseapp.com",
+"databaseURL": "https://espb.firebaseio.com",
 "projectId": "esp32-aleatorios",
 "storageBucket": "esp32-aleatorios.appspot.com",
-"messagingSenderId": "197506906640",
-"appId": "1:197506906640:web:160bd9370c5300a125e895",
-"measurementId": "G-GH5PT1E3Z1"
+"messagingSenderId": "1970",
+"appId": "1:197640:web:160b0a125e895",
+"measurementId": "G-GH5Z1"
 }
 
 firebase = pyrebase.initialize_app(config)
